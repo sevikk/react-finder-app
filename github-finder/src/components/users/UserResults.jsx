88a73e292c4a30393f046c44cdb28c170b2a5de7
Vsevolod.Kuznietsov <Vsevolod.Kuznietsov@texascapitalbank.com>
2022-12-30 22:09:30 -0600

    initial commit

---
+++ github-finder/src/components/users/UserResults.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+
+function UserResults() {
+  const { users, loading } = useContext(GithubContext);
+  
+
+  if (!loading) {
+    return (
+      <div className="grid frid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:frid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
